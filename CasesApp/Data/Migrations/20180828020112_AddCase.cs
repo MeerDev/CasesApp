fbad742aac2e32ce149c15ccb2055b9aaec5a448
Kevin Hall <Hallke88@hotmail.com>
2018-08-27 19:04:20 -0700

    Added CaseService and AddCase

---
+++ CasesApp/Data/Migrations/20180828020112_AddCase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CasesApp.Data.Migrations
+{
+    public partial class AddCase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Case",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Details = table.Column<string>(nullable: true),
+                    Worker = table.Column<string>(nullable: true),
+                    Reviewer = table.Column<string>(nullable: true),
+                    Approver = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Case", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Case");
+        }
+    }
+}
---
+++ CasesApp/Services/CaseService.cs
@@ -0,0 +1,26 @@
+﻿using CasesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasesApp.Services
+{
+    public interface ICaseService
+    {
+        Case Add { get; set; }
+
+        Case Review { get; set; }
+
+        Case Approve { get; set; }
+
+
+    }
+
+    public class CaseService : ICaseService
+    {
+        public Case Add { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Case Review { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Case Approve { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
