2914b789be6c2ebedcdec2d077011fa08af43305
Kevin Hall <Hallke88@hotmail.com>
2018-08-29 15:29:09 -0700

    Added account emails to Case model and changed index and details view to show emails instead of IDs

---
+++ CasesApp/Data/Migrations/20180829222529_emails.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CasesApp.Data.Migrations
+{
+    public partial class emails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApproverEmail",
+                table: "Case",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewerEmail",
+                table: "Case",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkerEmail",
+                table: "Case",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApproverEmail",
+                table: "Case");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewerEmail",
+                table: "Case");
+
+            migrationBuilder.DropColumn(
+                name: "WorkerEmail",
+                table: "Case");
+        }
+    }
+}
--- CasesApp/Models/Case.cs
+++ CasesApp/Models/Case.cs
@@ -12,8 +12,11 @@ namespace CasesApp.Models
         public string Title { get; set; }
         public string Details { get; set; }
         public string WorkerID { get; set; }
-        public string ReviewerID { get; set; }       
+        public string WorkerEmail { get; set; }
+        public string ReviewerID { get; set; }  
+        public string ReviewerEmail { get; set; }
         public string ApproverID { get; set; }
+        public string ApproverEmail { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime? DateReviewed { get; set; }
         public DateTime? DateApproved { get; set; }
