29c5b0794d3708728d2c3d1af63b4eec917ba586
Kevin Hall <Hallke88@hotmail.com>
2018-08-30 07:34:45 -0700

    PercentOfApprovedCasesPerUser change

--- CasesApp/SQL/PercentOfApprovedCasesPerUser.sql
+++ CasesApp/SQL/PercentOfApprovedCasesPerUser.sql
@@ -1,13 +1,24 @@
 CREATE PROCEDURE PercentOfApprovedCasesPerUser
 AS
 BEGIN
-	SELECT 
-		anu.UserName, 
-		COUNT(*) AS Cases,
-		(CONVERT(DECIMAL,COUNT(*))/CONVERT(DECIMAL,(SELECT COUNT(*) FROM dbo.[Case]))) * 100 AS 'Percent'
-	FROM dbo.[Case] AS c
-	JOIN dbo.AspNetUsers AS anu ON c.WorkerID = anu.Id
-	WHERE c.Status = 3
-	GROUP BY anu.UserName
+	SELECT
+	 t1.UserName, t2.ApprovedCases, t2.PercentOfApproved
+	 FROM
+		(SELECT DISTINCT
+			anu.UserName as UserName
+		FROM dbo.[Case] AS c
+		JOIN dbo.AspNetUsers AS anu ON c.WorkerID = anu.Id) t1
+	LEFT JOIN
+		(SELECT 
+			anu.UserName, 
+			COUNT(*) AS ApprovedCases,
+			(CONVERT(DECIMAL,COUNT(*))/CONVERT(DECIMAL,(SELECT COUNT(*) FROM dbo.[Case]))) * 100 AS PercentOfApproved
+		FROM dbo.[Case] AS c
+		JOIN dbo.AspNetUsers AS anu ON c.WorkerID = anu.Id
+		WHERE c.Status = 3
+		GROUP BY anu.UserName) t2
+	ON t1.UserName = t2.UserName
+
+
 END
 GO
