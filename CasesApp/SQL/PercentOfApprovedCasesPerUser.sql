f0a814fd04a54b532d51256509675a0d6a6d4e87
Kevin Hall <Hallke88@hotmail.com>
2018-08-30 06:56:37 -0700

    Added PercentOfApprovedCasesPerUser stored proc

---
+++ CasesApp/SQL/PercentOfApprovedCasesPerUser.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE PercentOfApprovedCasesPerUser
+AS
+BEGIN
+	SELECT 
+		anu.UserName, 
+		COUNT(*) AS Cases,
+		(CONVERT(DECIMAL,COUNT(*))/CONVERT(DECIMAL,(SELECT COUNT(*) FROM dbo.[Case]))) * 100 AS 'Percent'
+	FROM dbo.[Case] AS c
+	JOIN dbo.AspNetUsers AS anu ON c.WorkerID = anu.Id
+	GROUP BY anu.UserName
+END
+GO
