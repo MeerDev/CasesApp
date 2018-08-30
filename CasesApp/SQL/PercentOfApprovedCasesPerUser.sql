c2443695f8ad5595356eb43ef5982de134ee15b3
Kevin Hall <Hallke88@hotmail.com>
2018-08-30 07:14:51 -0700

    WHERE added to percentofapprovedcases

--- CasesApp/SQL/PercentOfApprovedCasesPerUser.sql
+++ CasesApp/SQL/PercentOfApprovedCasesPerUser.sql
@@ -7,6 +7,7 @@ BEGIN
 		(CONVERT(DECIMAL,COUNT(*))/CONVERT(DECIMAL,(SELECT COUNT(*) FROM dbo.[Case]))) * 100 AS 'Percent'
 	FROM dbo.[Case] AS c
 	JOIN dbo.AspNetUsers AS anu ON c.WorkerID = anu.Id
+	WHERE c.Status = 3
 	GROUP BY anu.UserName
 END
 GO
